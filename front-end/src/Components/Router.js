048d5d05bb2c07ed7d47307ad102e9dbc3eff28b
bae-sh <b5460881@naver.com>
2021-07-23 20:04:04 +0900

    게시판 수정

--- front-end/src/Components/Router.js
+++ front-end/src/Components/Router.js
@@ -1,10 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import {
-    BrowserRouter as Router,
-    Route,
-    Switch,
-    Redirect,
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
 } from "react-router-dom";
 
 import Board from "../Routes/Board";
@@ -19,20 +19,20 @@ import Login from "../Routes/Login";
 import Header from "../Components/Header";
 
 export default () => (
-    <Router>
-        <Header />
-        <main>
-            <Switch>
-                <Route path="/" component={Home} exact />
-                <Route path="/intro" component={Intro} exact />
-                <Route path="/coin" component={Coin} exact />
-                <Route path="/leaderboard" component={LeaderBoard} exact />
-                <Route path="/submit" component={Submit} exact />
-                <Route path="/board" component={Board} exact />
-                <Route path="/question" component={Question} exact />
-                <Route path="/login" component={Login} exact />
-                <Redirect to="/" />
-            </Switch>
-        </main>
-    </Router>
+  <Router>
+    <Header />
+    <main>
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/intro" component={Intro} exact />
+        <Route path="/coin" component={Coin} exact />
+        <Route path="/leaderboard" component={LeaderBoard} exact />
+        <Route path="/submit" component={Submit} exact />
+        <Route path="/board" component={Board} exact />
+        <Route path="/question" component={Question} exact />
+        <Route path="/login" component={Login} exact />
+        <Redirect to="/" />
+      </Switch>
+    </main>
+  </Router>
 );
--- front-end/src/Routes/Board/Presenter.js
+++ front-end/src/Routes/Board/Presenter.js
@@ -1,13 +1,109 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Main = styled.div`
-    padding: 40px 0;
+  padding: 30px 100px;
+  background-color: #e9ebf0;
+  height: 1000px;
 `;
 
+const MainContainer = styled.div`
+  background-color: white;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  margin: 0 100px;
+  & > h1 {
+    padding: 100px 0 30px 0;
+    font-size: 30px;
+    font-weight: 600;
+    border-bottom: 2px solid #a0a0a0;
+  }
+`;
+const Table = styled.table`
+  border: 1px solid #333;
+  width: 100%;
+  margin-top: 30px;
+  & > thead {
+    background-color: #e9ebf0;
+    color: black;
+    & > tr {
+      & > th {
+        padding: 20px;
+        font-weight: 500;
+      }
+      & > th:first-child {
+        width: 700px;
+      }
+    }
+  }
+  & > tbody {
+    & > tr {
+      border: 1px solid #333;
+      & > td {
+        padding: 10px 20px;
+        &:not(:first-child) {
+          text-align: center;
+        }
+      }
+    }
+  }
+`;
+const NoticeText = styled.span`
+  color: blue;
+  font-weight: 400;
+  padding-right: 5px;
+`;
 export default () => (
-    <Main>
-        <div className="inner">게시판</div>
-    </Main>
+  <Main>
+    <MainContainer>
+      <Container>
+        <h1>게시판</h1>
+        <Table>
+          <thead>
+            <tr>
+              <th colspan="1">제목</th>
+              <th colspan="1">등록일</th>
+              <th colspan="1">조회수</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Link>
+                  <NoticeText>[공지]</NoticeText>
+                  공지사항 입니다
+                </Link>
+              </td>
+              <td>2021-07-23</td>
+              <td>42</td>
+            </tr>
+            <tr>
+              <td>
+                <Link>
+                  <NoticeText>[공지]</NoticeText>
+                  공지사항 입니다
+                </Link>
+              </td>
+              <td>2021-07-23</td>
+              <td>42</td>
+            </tr>
+            <tr>
+              <td>
+                <Link>
+                  <NoticeText>[공지]</NoticeText>
+                  공지사항 입니다
+                </Link>
+              </td>
+              <td>2021-07-23</td>
+              <td>42</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </MainContainer>
+  </Main>
 );
