bc2a3cea7bc173b1cd3f131019e7da0762261321
bae-sh <b5460881@naver.com>
2021-07-24 18:04:17 +0900

    제출현황 수정

--- front-end/src/Routes/Submit/Container.js
+++ front-end/src/Routes/Submit/Container.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
 import Presenter from "./Presenter";
 
-export default () => <Presenter />;
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export default () => <Presenter data={data} />;
--- front-end/src/Routes/Submit/Presenter.js
+++ front-end/src/Routes/Submit/Presenter.js
@@ -1,16 +1,18 @@
 /* eslint-disable import/no-anonymous-default-export */
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const Main = styled.div`
   padding: 30px 100px;
-  background-color: #e9ebf0;
-  height: 1000px;
+  background-color: #e9ebf0; //회색
+  min-height: 1000px;
 `;
 
 const MainContainer = styled.div`
   background-color: white;
   height: 100%;
+  padding-bottom: 160px;
 `;
 
 const Container = styled.div`
@@ -67,39 +69,119 @@ const SubmitButton = styled.button`
   color: white;
   margin-top: 80px;
 `;
-export default () => (
-  <Main>
-    <MainContainer>
-      <Container>
-        <h1>제출</h1>
-        <Caution>
-          <li>
-            <Dot>·</Dot>한 번 제출된 데이터는 수정할 수 없습니다.
-          </li>
-          <li>
-            <Dot>·</Dot>제출시 신중하게 확인 후 제출해주시길 바랍니다.
-          </li>
-          <li>
-            <Dot>·</Dot>제출은 팀당 한 번만 제출할 수 있습니다.
-          </li>
-          <li>
-            <Dot>·</Dot>제출시 파일명 양식을 꼭 지켜주시길 바랍니다.
-          </li>
-          <li>
-            <Dot>·</Dot>파일명 양식: 0일차_팀명.xlsx
-          </li>
-        </Caution>
-        <Submit>
-          <h2>7일차 결과 데이터 제출 </h2>
-          <input type="text"></input>
-          <span>
-            <FileButton>파일첨부</FileButton>
-          </span>
-          <div>
-            <SubmitButton>제출</SubmitButton>
-          </div>
-        </Submit>
-      </Container>
-    </MainContainer>
-  </Main>
-);
+
+const OnSubmitButton = styled.button`
+  margin: 20px 0;
+  padding: 0 10px;
+  font-size: 20px;
+  height: 20px;
+  color: ${(props) => (props.onSubmit === true ? "black" : "#a0a0a0")};
+`;
+const Line = styled.span`
+  height: 10px;
+  border-right: 2px solid;
+`;
+const Box = styled.div`
+  margin: 70px 20px 30px 20px;
+  border: 2px solid #a0a0a0;
+  border-radius: 10px;
+  & > div {
+    padding: 15px 80px;
+    border-bottom: 1px solid #a0a0a0; //회색;
+    text-align: center;
+  }
+`;
+const Contents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+export default ({ data }) => {
+  let [onSubmit, setOnSubmit] = useState(true);
+  return (
+    <Main>
+      <MainContainer>
+        <Container>
+          <h1>제출</h1>
+          <OnSubmitButton
+            onSubmit={onSubmit}
+            onClick={() => {
+              setOnSubmit(true);
+            }}
+          >
+            제출
+          </OnSubmitButton>
+          <Line />
+          <OnSubmitButton
+            onSubmit={!onSubmit}
+            onClick={() => {
+              setOnSubmit(false);
+            }}
+          >
+            제출 현황
+          </OnSubmitButton>
+
+          {onSubmit === true ? (
+            <SubmitPage setOnSubmit={setOnSubmit} />
+          ) : (
+            <SubmitStatusPage data={data} />
+          )}
+        </Container>
+      </MainContainer>
+    </Main>
+  );
+};
+function SubmitPage() {
+  return (
+    <>
+      <Caution>
+        <li>
+          <Dot>·</Dot>한 번 제출된 데이터는 수정할 수 없습니다.
+        </li>
+        <li>
+          <Dot>·</Dot>제출시 신중하게 확인 후 제출해주시길 바랍니다.
+        </li>
+        <li>
+          <Dot>·</Dot>제출은 팀당 한 번만 제출할 수 있습니다.
+        </li>
+        <li>
+          <Dot>·</Dot>제출시 파일명 양식을 꼭 지켜주시길 바랍니다.
+        </li>
+        <li>
+          <Dot>·</Dot>파일명 양식: 0일차_팀명.xlsx
+        </li>
+      </Caution>
+      <Submit>
+        <h2>7일차 결과 데이터 제출 </h2>
+        <input type="text"></input>
+        <span>
+          <FileButton>파일첨부</FileButton>
+        </span>
+        <div>
+          <SubmitButton>제출</SubmitButton>
+        </div>
+      </Submit>
+    </>
+  );
+}
+
+function SubmitStatusPage(props) {
+  return (
+    <Contents>
+      {props.data.map((e, idx) => {
+        return (
+          <Box>
+            <div>{idx + 1}일차</div>
+            <div>
+              <Link>
+                <div style={{ color: "#a0a0a0", borderBottom: "1px solid" }}>
+                  download
+                </div>
+              </Link>
+            </div>
+          </Box>
+        );
+      })}
+    </Contents>
+  );
+}
