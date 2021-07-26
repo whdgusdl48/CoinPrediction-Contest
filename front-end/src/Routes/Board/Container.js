4ad6abeb475d32eaf32a72ed7b204b421c933478
whdgusdl48 <52066712+whdgusdl48@users.noreply.github.com>
2021-07-26 16:53:07 +0900

    No Changing

--- front-end/src/Routes/Board/Container.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import Presenter from "./Presenter";
-
-export default () => <Presenter />;
--- front-end/src/Routes/Coin/index.js
+++
@@ -1,4 +0,0 @@
-/* eslint-disable import/no-anonymous-default-export */
-import Container from "./Container";
-
-export default () => <Container />;
